7c543355ae117edc79b1b69404f3e396e96fbcaa
molla2021 <moget994@gmail.com>
2022-01-02 20:43:06 +0300

    first commit

---
+++ src/components/TripList.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect } from "react";
+import './TripList.css';
+
+export default function TripList() {
+     const [trips, setTrips] = useState([])
+     const [url, setUrl] = useState('http://localhost:3000/trips')
+
+    useEffect(() =>{
+        fetch(url)
+        .then(response => response.json())
+        .then(json => setTrips(json))
+    }, [url])
+    
+    console.log(trips)
+    return (
+        <div className="trip-list">
+           <h2>Trip List</h2>
+           <ui>
+               {trips.map(trip =>(
+                   <li key={trip.id}>
+                       <h3>{trip.title}</h3>
+                       <p>{trip.price}</p>
+                   </li>
+               ))}
+           </ui> 
+           <div className="filters">
+               <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+                   European Trips
+               </button>
+               <button onClick={() => setUrl('http://localhost:3000/trips')}>
+                   All Trips
+               </button>
+           </div>
+        </div>
+    )
+}
