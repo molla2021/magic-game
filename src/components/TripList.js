c9fbd1e680881282e4af4bbd97977cdae996a8bb
molla2021 <moget994@gmail.com>
2022-01-03 22:29:42 +0300

    added custom hook

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,36 +1,32 @@
-import React, {useState, useEffect } from "react";
-import './TripList.css';
+import { useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
+import './TripList.css'
 
 export default function TripList() {
-     const [trips, setTrips] = useState([])
-     const [url, setUrl] = useState('http://localhost:3000/trips')
+  const [url, setUrl] = useState('http://localhost:3000/trips')
+  const {data: trips, isPending} = useFetch(url)
+  
 
-    useEffect(() =>{
-        fetch(url)
-        .then(response => response.json())
-        .then(json => setTrips(json))
-    }, [url])
-    
-    console.log(trips)
-    return (
-        <div className="trip-list">
-           <h2>Trip List</h2>
-           <ui>
-               {trips.map(trip =>(
-                   <li key={trip.id}>
-                       <h3>{trip.title}</h3>
-                       <p>{trip.price}</p>
-                   </li>
-               ))}
-           </ui> 
-           <div className="filters">
-               <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
-                   European Trips
-               </button>
-               <button onClick={() => setUrl('http://localhost:3000/trips')}>
-                   All Trips
-               </button>
-           </div>
-        </div>
-    )
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      {isPending && <div>Loading trips...</div>}
+      <ul>
+        {trips && trips.map(trip => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
+    </div>
+  )
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect} from 'react';
+export const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async() => {
+            setIsPending(true)
+            const res = await fetch(url)
+            const data = await res.json()
+
+            setIsPending(false)
+            setData(data)
+        }
+        fetchData()
+    }, [url])
+    return { data, isPending }
+}
